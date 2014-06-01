37e673580209c4b79d6911a3b2f636ded43deae5
Thialfihar <thi@thialfihar.org>
2014-06-01 13:57:25 +0200

    Use Python3, add map analysis

--- mphi.py
+++ mphi.py
@@ -1,11 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import json
+import locale
 import math
 import subprocess
 from sympy import symbols, lcm
 
+encoding = locale.getdefaultlocale()[1]
+
 def factor(n):
     factors = {}
-    for p in subprocess.check_output(["factor", str(n)]).split(':')[1].strip().split(' '):
+    for p in subprocess.check_output(["factor", str(n)]).decode(encoding).split(':')[1].strip().split(' '):
         p = int(p)
         factors[p] = factors.get(p, 0) + 1
 
@@ -28,7 +32,6 @@ def lambda_n(n, k):
     for i in range(1, n + 1):
         term = lcm(term, x ** i - 1)
 
-    #print term
     for p, e in factors:
         result = lcm(result, int(p ** (e + math.floor(math.log(n) / math.log(p))) * term.evalf(subs={'x': p})))
 
@@ -61,14 +64,39 @@ def divisors(n):
 def lambdas():
     for k in range(2, 10):
         for n in range(2, 10):
-            print "lambda(n=%d, k=%d) = %s" % (n, k, lambda_n(n, k))
+            print("lambda(n=%d, k=%d) = %s" % (n, k, lambda_n(n, k)))
 
 def phis():
     for k in range(2, 10):
         for n in range(2, 10):
-            print "phi(n=%d, k=%d) = %s" % (n, k, phi_n(n, k))
+            print("phi(n=%d, k=%d) = %s" % (n, k, phi_n(n, k)))
 
-#phis()
-phi = phi_n(15, 12)
-print phi
-print lambda_n(15, 12)
+def analyse_map(n, k):
+    data = json.load(open('map_%d_%d.json' % (n, k), 'r'))
+    factors = factor(k)
+    phi = phi_n(n, k)
+    x = symbols('p')
+    terms = []
+    for i in range(1, n + 1):
+        terms = [x ** i - 1] + terms
+
+    exponents = map(str, sorted(map(int, data['map'].keys())))
+    for exponent in exponents:
+        num_matrices = data['map'][exponent]
+        exponent = int(exponent)
+        print(exponent)
+        tmp = exponent
+        tmp2 = 0
+        while tmp > 1 and tmp % p == 0:
+            tmp /= p
+            tmp2 += 1
+        if tmp2 > 0:
+            print("    = %s * %s [%s]" % (tmp, exponent / tmp, 'p**%d' % tmp2))
+
+        for p, e in factors:
+            for term in terms:
+                f = int(term.evalf(subs={'p': p}))
+                if exponent % f == 0:
+                    print("    = %s * %s [%s]" % (exponent / f, f, term))
+
+analyse_map(3, 3)
